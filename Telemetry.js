60a1ea74318866a69d7db7233c587ee718ce4aa1
Jay Park <jay.park1921@gmail.com>
2022-02-01 12:54:35 +0900

    Added profiler work

---
+++ Telemetry.js
@@ -0,0 +1,27 @@
+const axios = require('axios')
+
+class Telemetry {
+  constructor(httpOptions) {
+    this.axios = axios.create(httpOptions)
+  }
+
+  async send(data) {
+    try {
+      await this.axios.post(this.options.url, data); // user should specify 
+    } catch (err) {
+      console.log('An error occurred sending telemetry data', err)
+    }
+  }
+}
+
+module.exports = { Telemetry }
+
+
+
+
+
+
+
+
+
+
---
+++ tests/profiler.spec.js
@@ -0,0 +1,56 @@
+const { Profiler } = require('../Profiler')
+
+describe('test profiler', () => {
+
+  test('profiler should run function', () => {
+    const profiler = new Profiler();
+
+    const testFunction = jest.fn(() => {
+      let count = 0;
+      for (let i = 0; i < 999999999; i++) {
+        count++
+      }
+    });
+    profiler.measure(testFunction);
+
+    expect(testFunction).toHaveBeenCalled();
+  });
+
+  test('should be usable', () => {
+    const profiler = new profiler({
+      /**
+       * what do i want to go here?
+       * such that it changes the module's behavior
+       * and how should the behavior change?
+       */
+    });
+
+    profiler.measure({
+      /**
+       * what do i want to go here?
+       * such that it changes the function's behavior
+       * and how should the behavior change?
+       */
+    }, () => {}, () => {})
+
+    function callOne() {
+      callTwo()
+    }
+
+    function callTwo() {
+    }
+
+  });
+
+  test('Only runs if env is set', () => {
+    process.env.SPECIAL_ENV_VAR='';
+    const profiler = new profiler({
+    });
+
+    const testFunction = jest.fn()
+    profiler.measure({
+    }, testFunction)
+
+    expect(testFunction).not.toHaveBeenCalled()
+  });
+})
